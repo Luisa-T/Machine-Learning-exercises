df69bedbe0405665beefc6018fc17e6638716505
Luisa <G00364698@gmit.ie>
2019-09-17 10:52:56 +0100

    First draft of Collatz

---
+++ Collatz.py
@@ -0,0 +1,18 @@
+# first week exercise
+
+n = 20
+
+# keep looping until we reach 1
+#Note: ths assumes that the Collatx conjecture is true
+while n != 1:
+    # print the current value of n
+    print(n)
+    # check if n is even
+    if n % 2 == 0:
+        # if n is even, divide it by two
+        n = n / 2
+    else:
+        # if n is odd, multiply be three and add 1
+        n = ( 3 * n) + 1
+# Finally, print 1
+print(n)
